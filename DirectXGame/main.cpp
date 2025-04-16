f9169e6cce1f75acfcb3ac6ee4483f87047e8370
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-04-16 11:38:47 +0900

    KamataEngineを入れた

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,45 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
+{
+	
+	KamataEngine::Initialize(L"LE2C_09_コバヤシ_トキヤ_AL3");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+
+	while (true) 
+	{
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+		//ゲームシーンの更新
+		gameScene->Update();
+		
+		//描画開始
+		dxCommon->PreDraw();
+		
+		//ゲームシーンの描画
+		gameScene->Draw();
+		
+		//描画終了
+		dxCommon->PostDraw();
+		
+		//ゲームシーンの解放
+		delete gameScene;
+		//nullptrの代入
+		gameScene = nullptr;
+		
+	}
+	KamataEngine::Finalize();
+
+	return 0;
+}
