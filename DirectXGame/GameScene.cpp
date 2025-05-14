4fddd46b06b0a26397044013ce8c08c18a2f147f
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-05-14 11:59:34 +0900

    謎のエラー

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,17 +2,94 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize()
-{
+GameScene::~GameScene() {
+	delete model_;
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+			delete worldTransformBlock;
+		}
+	}
+	worldTransformBlocks_.clear();
+
+	delete debugCamera_;
+}
+
+void GameScene::Initialize() {
+	model_ = Model::Create();
+
+	camera_.Initialize();
+
+	const uint32_t kNumBlockVirtical = 10;
+	const uint32_t kNumBlockHorizontal = 20;
+
+	const float kBlockWidth = 2.0f;
+	const float kBlockHeight = 2.0f;
+
+	worldTransformBlocks_.resize(kNumBlockHorizontal);
+	worldTransformBlocks_.resize(kNumBlockVirtical);
+
+	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
+	}
+
+	for (uint32_t i = 0; i < kNumBlockVirtical; i++) {
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+			if ((i + j) % 2 == 1)
+				continue;
+			worldTransformBlocks_[i][j] = new WorldTransform();
+			worldTransformBlocks_[i][j]->Initialize();
+			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
+			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
+		}
+	}
+
+	debugCamera_ = new DebugCamera(1280,720 );
 
 }
 
-void GameScene::Update()
-{
+void GameScene::Update() {
 
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+			if (!worldTransformBlock)
+				continue;
+
+			worldTransformBlock->matWorld_ = MakeAffineMatrix(worldTransformBlock->scale_, worldTransformBlock->rotation_, worldTransformBlock->translation_);
+			worldTransformBlock->TransferMatrix();
+		}
+	}
+
+	debugCamera_->Update();
+#ifdef _DEBUG
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE))
+	{
+		isDebugCameraActive_ = !isDebugCameraActive_;
+	}
+#endif // _DEBUG
+	if (isDebugCameraActive_)
+	{
+		debugCamera_->Update();
+		camera_.matView = debugCamera_->GetCamera().matView;
+		camera_.matProjection = debugCamera_->GetCamera().matProjection;
+	}
+	else
+	{
+		camera_.UpdateMatrix();
+	}
 }
 
-void GameScene::Draw()
-{
+void GameScene::Draw() {
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	Model::PreDraw(dxCommon->GetCommandList());
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+
+			if (!worldTransformBlock)
+				continue;
+
+			model_->Draw(*worldTransformBlock, camera_, nullptr);
+		}
+	}
+	Model::PostDraw();
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "KamataEngine.h"
+#include "vector"
+#include "Math.h"
 
 class GameScene 
 {
 public:
+	~GameScene();
 	//初期化
 	void Initialize();
 
@@ -12,4 +15,17 @@ public:
 
 	//描画
 	void Draw();
+private:
+	KamataEngine::Model* model_ = nullptr;
+
+	KamataEngine::Camera camera_;
+
+	std::vector < std::vector<KamataEngine::WorldTransform* >> worldTransformBlocks_;
+
+	bool isDebugCameraActive_ = false;
+
+	DebugCamera* debugCamera_ = nullptr;
+
+	
+
 };
