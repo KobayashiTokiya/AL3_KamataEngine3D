2da25f723e3e346fc429c2b08c971a6ae1197030
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-06-18 12:24:35 +0900

    p15まで書き、実行すると一応慣性があるような動きはしているが一方方向のキーを押していると、少し突っかかるみたいな感じになっている

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,28 +1,59 @@
 #include "Player.h"
 #include <cassert>
 
-void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position)
-{
+void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position) {
 	assert(model);
 
 	model_ = model;
 
-	//textureHandle_ = textureHandle;
+	// textureHandle_ = textureHandle;
 
 	worldTransform_.Initialize();
-	//プレイヤーの初期の座標と回転
+	// プレイヤーの初期の座標と回転
 	worldTransform_.translation_ = position;
 	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
 
 	camera_ = camera;
 }
 
-void Player::Update() 
-{ 
+void Player::Update() {
+	if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) 
+	{
+		Vector3 acceleration = {};
+		if (Input::GetInstance()->PushKey(DIK_RIGHT))
+		{
+			// 左移動中の右入力
+			if (velocity_.x < 0.0f) 
+			{
+				// 速度の逆方向に入力中は急ブレーキ
+				velocity_.x *= (1.0f - kAttenuation);
+			}
+			acceleration.x += kAcceleration;
+		}
+		else if (Input::GetInstance()->PushKey(DIK_LEFT))
+		{
+			// 右移動中の左入力
+			if (velocity_.x > 0.0f)
+			{
+				// 速度の逆方向に入力中は急ブレーキ
+				velocity_.x *= (1.0f - kAttenuation);
+			}
+			acceleration.x -= kAcceleration;
+		}
+		else 
+		{
+			velocity_.x *= (1.0f - kAttenuation);
+		}
+
+		// 加速/減速
+		velocity_ += acceleration;
+		// 最大速度制限
+		velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+	}
+
+	worldTransform_.translation_ += velocity_;
+
 	WorldTransformUpdate(worldTransform_);
 }
 
-void Player::Draw() 
-{ 
-	model_->Draw(worldTransform_, *camera_); 
-}
+void Player::Draw() { model_->Draw(worldTransform_, *camera_); }
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -3,15 +3,18 @@
 #include "Math.h"
 //std::numbers::pi_vを使うためにnumbersをインクルードをする
 #include <numbers>
+//std::clamp()を使うためのもの
+#include <algorithm>
 
-class Player
-{
+class Player {
 public:
 	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position);
 
 	void Update();
 
 	void Draw();
+
+	Vector3 velocity_ = {};
 
 private:
 	WorldTransform worldTransform_;
@@ -21,4 +24,11 @@ private:
 	uint32_t textureHandle_ = 0u;
 
 	Camera* camera_ = nullptr;
+	//02_05 フレームごとの加速度
+	static inline const float kAcceleration = 0.01f;
+	//02_05 非入力時の摩擦係数
+	static inline const float kAttenuation = 0.05f;
+	//02_05 最低/最大速度の制限のため
+	static inline const float kLimitRunSpeed = 0.3f;
+
 };
