7fdacdc2cf3a9d98a760291416a7be2dcc89bd72
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-06-20 11:12:02 +0900

    プレイヤーの移動とジャンプができた。

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,7 +1,10 @@
+// std::maxを使うためのもの
+#define NOMINMAX
+
 #include "Player.h"
 #include <cassert>
 
-void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position) {
+void Player::Initialize(Model* model, Camera* camera, const Vector3& position) {
 	assert(model);
 
 	model_ = model;
@@ -17,48 +20,114 @@ void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera
 }
 
 void Player::Update() {
-	if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
-		Vector3 acceleration = {};
-		if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
-			// 左移動中の右入力
-			if (velocity_.x < 0.0f) {
-				// 速度の逆方向に入力中は急ブレーキ
-				velocity_.x *= (1.0f - kAttenuation);
-			}
-			acceleration.x += kAcceleration;
+	// 接地状態
+	if (onGround_) {
+		if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
+			// 左右加速
+			Vector3 acceleration = {};
+			if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+				// 左移動中の右入力
+				if (velocity_.x < 0.0f) {
+					// 速度の逆方向に入力中は急ブレーキ
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+				acceleration.x += kAcceleration;
 
-			if (lrDirection_ != LRDirection::kRight) {
-				lrDirection_ = LRDirection::kRight;
+				if (lrDirection_ != LRDirection::kRight) {
+					lrDirection_ = LRDirection::kRight;
+					// 旋回開始時の角度を記録する
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					// 旋回タイマーを時間を設定する
+					turnTimer_ = kTimeTurn;
+				}
 			}
-		} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
-			// 右移動中の左入力
-			if (velocity_.x > 0.0f) {
-				// 速度の逆方向に入力中は急ブレーキ
-				velocity_.x *= (1.0f - kAttenuation);
-			}
-			acceleration.x -= kAcceleration;
 
-			if (lrDirection_ != LRDirection::kLeft) {
-				lrDirection_ = LRDirection::kLeft;
+			else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+				// 右移動中の左入力
+				if (velocity_.x > 0.0f) {
+					// 速度と逆方向に入力中は急ブレーキ
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+
+				acceleration.x -= kAcceleration;
+
+				if (lrDirection_ != LRDirection::kLeft) {
+					lrDirection_ = LRDirection::kLeft;
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
 			}
-		} else {
+			// 加速/減速
+			velocity_ += acceleration;
+			// 最大速度制限
+			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+		}
+		else 
+		{
+			// 非入力時に移動減衰をかける
 			velocity_.x *= (1.0f - kAttenuation);
 		}
 
-		// 加速/減速
-		velocity_ += acceleration;
-		// 最大速度制限
-		velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+		// ジャンプ入力
+		if (Input::GetInstance()->PushKey(DIK_UP)) {
+			// ジャンプ初速
+			velocity_ += Vector3(0, kJumpAcceleration / 60.0f, 0);
+		}
+
 	}
-	// 旋回制御
-	float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+	// 空中
+	else {
+		// 落下速度
+		velocity_ += Vector3(0, -kGravityAcceleration / 60.0f, 0);
+		// 落下速度制限
+		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
+	}
 
-	float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
-
-	worldTransform_.rotation_.y = destinationRotationY;
-	
-	
 	worldTransform_.translation_ += velocity_;
+
+	// 着地フラグ
+	bool landing = false;
+
+	// 地面との当たり判定
+	// 下降中？
+	if (velocity_.y < 0) {
+		// Y座標が地面以上になったら着地
+		if (worldTransform_.translation_.y <= 1.0f) {
+			landing = true;
+		}
+	}
+
+	// 接地判定
+	if (onGround_) {
+		// ジャンプ開始
+		if (velocity_.y > 0.0f) {
+			// 空中状態に以降
+			onGround_ = false;
+		}
+	} else {
+		// 着地
+		if (landing) {
+			// めり込み排斥
+			worldTransform_.translation_.y = 1.0f;
+			// 摩擦で横方向速度が減衰する
+			velocity_.x *= (1.0f - kAttenuation);
+			// 下方向速度をリセット
+			velocity_.y = 0.0f;
+			// 接地状態の移行
+			onGround_ = true;
+		}
+	}
+
+	// 旋回制御
+	if (turnTimer_ > 0.0f) {
+		turnTimer_ = std::max(turnTimer_ - (1.0f / 60.0f), 0.0f);
+
+		float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+
+		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+
+		worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
+	}
 
 	WorldTransformUpdate(worldTransform_);
 }
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,20 +1,20 @@
 #pragma once
 #include "KamataEngine.h"
 #include "Math.h"
-//std::numbers::pi_vを使うためにnumbersをインクルードをする
+// std::numbers::pi_vを使うためにnumbersをインクルードをする
 #include <numbers>
-//std::clamp()を使うためのもの
+// std::clamp()を使うためのもの
 #include <algorithm>
+
+using namespace KamataEngine;
 
 class Player {
 public:
-	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position);
+	void Initialize(Model* model, Camera* camera, const Vector3& position);
 
 	void Update();
 
 	void Draw();
-
-	Vector3 velocity_ = {};
 
 	enum class LRDirection
 	{
@@ -30,12 +30,28 @@ private:
 	uint32_t textureHandle_ = 0u;
 
 	Camera* camera_ = nullptr;
-	//02_05 フレームごとの加速度
-	static inline const float kAcceleration = 0.01f;
-	//02_05 非入力時の摩擦係数
-	static inline const float kAttenuation = 0.05f;
-	//02_05 最低/最大速度の制限のため
-	static inline const float kLimitRunSpeed = 0.3f;
 
+	Vector3 velocity_ = {};
+	// 02_05 フレームごとの加速度
+	static inline const float kAcceleration = 0.01f;
+	// 02_05 非入力時の摩擦係数
+	static inline const float kAttenuation = 0.05f;
+	// 02_05 最低/最大速度の制限のため
+	static inline const float kLimitRunSpeed = 0.3f;
+	//02_05 顔の向き
 	LRDirection lrDirection_ = LRDirection::kRight;
+	// 旋回開始時の角度
+	float turnFirstRotationY_ = 0.0f;
+	// 旋回タイマー
+	float turnTimer_ = 0.0f;
+	// 旋回時間<秒>
+	static inline const float kTimeTurn = 0.3f;
+	// 接地状態フラグ
+	bool onGround_ = true;
+	// 重力加速度(下方向)
+	static inline const float kGravityAcceleration = 0.98f;
+	// 最大落下速度(下方向)
+	static inline const float kLimitFallSpeed = 0.5f;
+	// ジャンプ初速(上方向)
+	static inline const float kJumpAcceleration = 20.0f;
 };
