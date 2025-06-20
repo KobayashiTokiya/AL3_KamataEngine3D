ee11919c34cac96f88448df58d1de79f45b2774f
KobayashiTokiya <k024g0090@g.neec.ac.jp>
2025-06-20 13:04:07 +0900

    p18の途中

--- DirectXGame/CameraController.h
+++ DirectXGame/CameraController.h
@@ -17,10 +17,26 @@ public:
 
 	void Reset();
 
+	void SetMovableArea();
+
+	struct Rect 
+	{
+		//左端
+		float left = 0.0f;
+		//右端
+		float right = 1.0f;
+		//下端
+		float bottom = 0.0f;
+		//上端
+		float top = 1.0f;
+	};
+
 private:
 	Camera* camera_ = nullptr;
 
 	Player* target_ = nullptr;
 	//追従対象とカメラの座標の差(オフセット)
 	Vector3 targetOffset_ = {0, 0, -15.0f};
+	//カメラ移動範囲
+	Rect movableArea_ = {0, 100, 0, 100};
 };
